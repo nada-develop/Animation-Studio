bbc3e5d89b1071e62cbaf237336357e178be71bc
Nada Gomaa <nadaengineer88@gmail.com>
2021-02-10 23:30:31 +0200

    Adding Front-end Routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+// authentication
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+//----frontendRoutes
+//loading the homepage
+Route::get('/','FrontController@homepage');
+//loading the aboutpage
+Route::get('/about','FrontController@about');
+//loading the contactpage
+Route::get('/contact','FrontController@contact');
+//loading portfolio items
+Route::get('/portfolio','PortfolioController@index');
+//loading blog posts
+Route::get('/blog','BlogController@index');
+//loading awards
+Route::get('/awards','AwardController@index');
+//loading a single portfolio item
+Route::get('/portfolio/{id}','PortfolioController@show');
+//loading a single blog post 
+Route::get('/blog/{id}','BlogController@show');
+//loading single award 
+Route::get('/awards/{id}','AwardController@show');
+//loading portfolio items for a specific category
+Route::get('/portfolio/{name}','PortfolioController@show_category');
+
+
+
+
+
